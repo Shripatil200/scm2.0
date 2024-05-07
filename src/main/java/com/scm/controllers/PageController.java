fc1dab8644dd33666c717eaa20bb149441438d70
Shripatil200 <patilshriram200@gmail.com>
2024-05-07 17:35:54 +0530

    first commit

---
+++ src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,38 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String home(Model model){
+        System.out.println("home page randler");
+        model.addAttribute("name", "Substring technologies");
+        model.addAttribute("message", "Learn spring boot");
+        model.addAttribute("githubRepo", "https://github.com/Shripatil200");
+        return "home";
+    }
+
+    // about rought
+
+    @RequestMapping("/about")
+    public String aboutPage(){
+        System.out.println("about page controller");
+        return "about";
+    }
+
+    //services
+    @RequestMapping("/services")
+    public String servicesPage(){
+        System.out.println("services page controller");
+        return "services";
+    }
+}
